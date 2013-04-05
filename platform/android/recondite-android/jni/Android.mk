d0fb58ecfeac1202892d2b9a1e1581d2011ae378
Matthew LaRocca <matthew@techsoft3d.com>
2013-04-05 11:29:36 -0700

    update android makefiles

--- platform/android/recondite-android/jni/Android.mk
+++ platform/android/recondite-android/jni/Android.mk
@@ -17,7 +17,57 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := recondite-android
+
+LOCAL_C_INCLUDES += ../../../include
+
 LOCAL_SRC_FILES := main.c
+
+LOCAL_SRC_FILES += ../../../../src/math/rMath.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rMathUtil.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rMatrix3.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rMatrix4.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rMatrixUtil.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rQuaternion.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rVector2.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rVector3.cpp
+LOCAL_SRC_FILES += ../../../../src/math/rVector4.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rAlignedBox3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rCircle2.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rCone3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rFrustrum.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rGeometry3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rIntersection2.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rIntersection3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rLine2.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rLine3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rPlane3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rRectangle2.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rSphere3.cpp
+LOCAL_SRC_FILES += ../../../../src/geometry/rTriangle2.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rActor3.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rCamera.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rColor.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rGameTime.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rGraphicsDevice.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rPoint.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rRect.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rSize.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rViewport.cpp
+LOCAL_SRC_FILES += ../../../../src/core/rContentManager.cpp
+LOCAL_SRC_FILES += ../../../../src/asset/rAsset.cpp
+LOCAL_SRC_FILES += ../../../../src/asset/rMaterial.cpp
+LOCAL_SRC_FILES += ../../../../src/asset/rTexture2D.cpp
+LOCAL_SRC_FILES += ../../../../src/asset/rShader.cpp
+LOCAL_SRC_FILES += ../../../../src/xml/rXMLDocument.cpp
+LOCAL_SRC_FILES += ../../../../src/xml/rXMLDocumentLoader.cpp
+LOCAL_SRC_FILES += ../../../../src/xml/rXMLWriter.cpp
+LOCAL_SRC_FILES += ../../../../src/xml/rXMLReader.cpp
+LOCAL_SRC_FILES += ../../../../src/xml/rXMLElement.cpp
+LOCAL_SRC_FILES += ../../../../src/xml/rXMLAttributeList.cpp
+LOCAL_SRC_FILES += ../../../../src/data/rTexture2DData.cpp
+LOCAL_SRC_FILES += ../../../../src/data/rMaterialData.cpp
+LOCAL_SRC_FILES += ../../../../src/data/rShaderData.cpp
+
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
--- platform/android/recondite-android/jni/Application.mk
+++ platform/android/recondite-android/jni/Application.mk
@@ -1,1 +1,3 @@
 APP_PLATFORM := android-10
+APP_STL := gnustl_static 
+APP_SHORT_COMMANDS := true

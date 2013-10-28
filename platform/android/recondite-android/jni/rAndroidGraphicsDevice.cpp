2aa36520816dc3997b919fdc3da924b013cfd9e9
Matthew LaRocca <matthew@techsoft3d.com>
2013-10-27 21:30:50 -0700

    request a depth buffer when making opengles2 canvas on android

--- platform/android/recondite-android/jni/rAndroidGraphicsDevice.cpp
+++ platform/android/recondite-android/jni/rAndroidGraphicsDevice.cpp
@@ -16,6 +16,7 @@ bool rAndroidGraphicsDevice::Init(android_app* state){
 			EGL_BLUE_SIZE, 8,
 			EGL_GREEN_SIZE, 8,
 			EGL_RED_SIZE, 8,
+			EGL_DEPTH_SIZE,16,
 			EGL_NONE
 	};
 	EGLint numConfigs;

4fa29f52216802ead7416fc9b4355ad30558d0bc
Matthew LaRocca <mlarocca.cpp@gmail.com>
2013-12-02 18:56:47 -0800

    user can view different models in android demo app

--- platform/android/recondite-android/jni/ruiDemoController.cpp
+++ platform/android/recondite-android/jni/ruiDemoController.cpp
@@ -1,43 +1,50 @@
 #include "ruiDemoController.hpp"
 
-ruiDemoController::ruiDemoController(rController* controller){
+ruiDemoController::ruiDemoController(rContentManager* contentManager, rController* controller){
 	m_controller = controller;
+	m_contentManager = contentManager;
 }
 
 void ruiDemoController::Init(ruiOverlay* overlay){
-	m_picker = new ruiPicker(102, rPoint(25,10), rSize(250, 35));
-	m_picker->Bind(ruiEVENT_PICKER_CHANGE, this, &ruiDemoController::OnPickerChange);
+	m_modelPicker = new ruiPicker(100, rPoint(25,10), rSize(250, 35));
 
-	m_slider = new ruiSlider(101, rPoint(25,75), rSize(250, 35));
-	m_slider->Bind(ruiEVENT_SLIDER_CHANGE, this, &ruiDemoController::OnSliderChange);
+	rArrayString models;
+	m_contentManager->GetModelNames(models);
+	m_modelPicker->SetOptions(models);
 
-	m_checkbox = new ruiCheckbox(667, rPoint(25, 195), rSize(50, 50));
-	m_checkbox->Bind(ruiEVENT_CHECKBOX_CHANGE, this, &ruiDemoController::OnCheckboxChange);
+	m_modelPicker->Bind(ruiEVENT_PICKER_CHANGE, this, &ruiDemoController::OnModelChange);
 
-	m_button = new ruiButton("click me", 666, rPoint(25, 120), rSize(175, 40));
-	m_button->Bind(ruiEVENT_BUTTON_CLICK, this, &ruiDemoController::OnButtonClick);
+	m_animationPicker = new ruiPicker(101, rPoint(25,60), rSize(250, 35));
 
-	overlay->AddWidget(new ruiDPad(m_controller->DPad(0), 100, rPoint(700, 300), rSize(300, 300)));
-	overlay->AddWidget(m_picker);
-	overlay->AddWidget(m_slider);
-	overlay->AddWidget(m_button);
-	overlay->AddWidget(m_checkbox);
-	overlay->AddWidget(new ruiText("Hello Android, How R U?\nDroid Does.", 900, rPoint(25, 275), rSize(200, 150)));
+	overlay->AddWidget(new ruiDPad(m_controller->DPad(0), 999, rPoint(700, 300), rSize(300, 300)));
+	overlay->AddWidget(m_modelPicker);
+	overlay->AddWidget(m_animationPicker);
+
+	if (models.size() > 0)
+		SetActiveModel(models[0]);
+
 }
 
-void ruiDemoController::OnButtonClick(ruiWidget* button){
-	rLog::Info("button %d clicked", button->Id());
+void ruiDemoController::OnModelChange(ruiWidget* widget){
+	ruiPicker* modelPicker = (ruiPicker*)widget;
+
+	SetActiveModel(modelPicker->SelectionText());
 }
 
-void ruiDemoController::OnPickerChange(ruiWidget* picker){
-	rLog::Info("picker %d changed", picker->Id());
+rString ruiDemoController::GetActiveModelName(){
+	return m_activeModelName;
 }
 
-void ruiDemoController::OnSliderChange(ruiWidget* slider){
-	ruiSlider* s = (ruiSlider*)slider;
-	rLog::Info("slider changed - value: %d", s->GetValue());
-}
+void ruiDemoController::SetActiveModel(const rString& name){
+	rArrayString animationNames;
+	rModel* model = m_contentManager->GetModelAsset(name);
+	rSkeleton* skeleton = model->Skeleton();
 
-void ruiDemoController::OnCheckboxChange(ruiWidget* checkbox){
-	rLog::Info("checkbox %d changed", checkbox->Id());
+	if (skeleton){
+		skeleton->GetAnimationNames(animationNames);
+	}
+
+	m_animationPicker->SetOptions(animationNames);
+
+	m_activeModelName = name;
 }

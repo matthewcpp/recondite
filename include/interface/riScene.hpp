6269105665dd82edf0a8c994820553f6d35d7c0a
matthewcpp <mlarocca.cpp@gmail.com>
2014-03-23 22:58:36 -0700

    add scene interface file

---
+++ include/interface/riScene.hpp
@@ -0,0 +1,14 @@
+#ifndef RI_SCENE_HPP
+#define RI_SCENE_HPP
+
+class rActor3;
+
+class riScene{
+public:
+	virtual void AddActor(rActor3* actor) = 0;
+	virtual rActor3* GetActor(const rString& name) const = 0;
+
+	virtual void DeleteActor(const rString& name) = 0;
+};
+
+#endif

d64dba6165ecffb820ec0f8a6897c8fe93839ba4
Matthew LaRocca <mlarocca.cpp@gmail.com>
2014-03-24 23:05:27 -0700

    cmake function to easily add source files

--- cmake/recondite.cmake
+++ cmake/recondite.cmake
@@ -1,7 +1,18 @@
 function(add_rec_src list_target name)
+ 	set (opt_params ${ARGN})
+ 	set (opt_params_length 0)
+	list(LENGTH opt_params opt_params_length)
+	
 	set(temp)
 	list(APPEND temp ${CMAKE_CURRENT_SOURCE_DIR}/${name}.cpp)
-	list(APPEND temp ${CMAKE_CURRENT_SOURCE_DIR}/../../include/${name}.hpp)
+
+	if (${opt_params_length} GREATER 0)
+		set(include_subdir)
+		list(GET opt_params 0 include_subdir)
+		list(APPEND temp ${CMAKE_CURRENT_SOURCE_DIR}/../../include/${include_subdir}/${name}.hpp)
+	else()
+		list(APPEND temp ${CMAKE_CURRENT_SOURCE_DIR}/../../include/${name}.hpp)
+	endif()
 	
 	set(${list_target} ${${list_target}} ${temp} PARENT_SCOPE)
 	

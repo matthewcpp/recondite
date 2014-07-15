557317c7fa408b7861fe9ba9a54a30bee20e931e
Matthew LaRocca <mlarocca.cpp@gmail.com>
2014-07-14 18:37:41 -0700

    log target for std out

---
+++ include/log/rLogStdOut.hpp
@@ -0,0 +1,20 @@
+#ifndef R_LOGSTDOUT_HPP
+#define R_LOGSTDOUT_HPP
+
+#include <iostream>
+
+#include "rBuild.hpp"
+#include "log/rLogTarget.hpp"
+
+class RECONDITE_API rLogStdOut : public rLogTarget{
+public:
+	virtual void Debug(const rString& message);
+	virtual void Warning(const rString& message);
+	virtual void Trace(const rString& message);
+	virtual void Info(const rString& message);
+	virtual void Error(const rString& message);
+
+	virtual void Shutdown();
+};
+
+#endif
---
+++ src/log/rLogStdOut.cpp
@@ -0,0 +1,25 @@
+#include "log/rLogStdOut.hpp"
+
+void rLogStdOut::Debug(const rString& message){
+	std::cout << message.c_str() << std::endl;
+}
+
+void rLogStdOut::Warning(const rString& message){
+	std::cout << message.c_str() << std::endl;
+}
+
+void rLogStdOut::Trace(const rString& message){
+	std::cout << message.c_str() << std::endl;
+}
+
+void rLogStdOut::Info(const rString& message){
+	std::cout << message.c_str() << std::endl;
+}
+
+void rLogStdOut::Error(const rString& message){
+	std::cout << message.c_str() << std::endl;
+}
+
+void rLogStdOut::Shutdown(){
+
+}

ea426e5e726779549bdc571a3dbe43d47a5728de
matthewcpp <mlarocca.cpp@gmail.com>
2014-09-14 13:04:27 -0700

    primitive box is a little more colorful

--- include/primitive/rPrimitiveBox.hpp
+++ include/primitive/rPrimitiveBox.hpp
@@ -8,12 +8,17 @@
 class RECONDITE_API rPrimitiveBox : public rActor3{
 public:
 	rPrimitiveBox(const rString& name, const rAlignedBox3& box);
+	rPrimitiveBox(const rString& name, const rAlignedBox3& box, const rColor& color);
 
 public:
 	virtual void Draw(rEngine& engine);
 
+	void SetColor(const rColor& color);
+	rColor Color() const;
+
 private:
 	rAlignedBox3 m_box;
+	rColor m_color;
 };
 
 #endif
--- src/primitive/rPrimitiveBox.cpp
+++ src/primitive/rPrimitiveBox.cpp
@@ -4,8 +4,24 @@ rPrimitiveBox::rPrimitiveBox(const rString& name, const rAlignedBox3& box)
 	:rActor3(name, rVector3::ZeroVector)
 {
 	m_box = box;
+	m_color = rColor::White;
+}
+
+rPrimitiveBox::rPrimitiveBox(const rString& name, const rAlignedBox3& box, const rColor& color)
+	:rActor3(name, rVector3::ZeroVector)
+{
+	m_box = box;
+	m_color = color;
 }
 
 void rPrimitiveBox::Draw(rEngine& engine){
-	engine.renderer->RenderWireBox(m_box, rColor::Red);
+	engine.renderer->RenderWireBox(m_box, m_color);
+}
+
+void rPrimitiveBox::SetColor(const rColor& color){
+	m_color = color;
+}
+
+rColor rPrimitiveBox::Color() const{
+	return m_color;
 }

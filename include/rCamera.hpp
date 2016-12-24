55ef4745b398b4dd8406a38615c2edcd852720da
Matthew LaRocca <mlarocca.cpp@gmail.com>
2016-12-23 17:57:15 -0800

    Add camera convenience methods

--- include/rCamera.hpp
+++ include/rCamera.hpp
@@ -10,12 +10,15 @@ namespace recondite {
 			m_position(rVector3::ZeroVector), m_target(rVector3::ForwardVector), m_up(rVector3::UpVector) {}
 
 		inline void SetPosition(const rVector3& position);
+		inline void SetPosition(float x, float y, float z);
 		inline rVector3 GetPosition() const;
 
 		inline void SetTarget(const rVector3& target);
+		inline void SetTarget(float x, float y, float z);
 		inline rVector3 GetTarget() const;
 
 		inline void SetUp(const rVector3& up);
+		inline void SetUp(float x, float y, float z);
 		inline rVector3 GetUp() const;
 
 	private:
@@ -28,6 +31,10 @@ namespace recondite {
 		m_position = position;
 	}
 
+	inline void Camera::SetPosition(float x, float y, float z) {
+		m_position.Set(x, y, z);
+	}
+
 	inline rVector3 Camera::GetPosition() const {
 		return m_position;
 	}
@@ -36,12 +43,20 @@ namespace recondite {
 		m_target = target;
 	}
 
+	inline void Camera::SetTarget(float x, float y, float z) {
+		m_target.Set(x, y, z);
+	}
+
 	inline rVector3 Camera::GetTarget() const {
 		return m_target;
 	}
 
 	inline void Camera::SetUp(const rVector3& up) {
 		m_up = up;
+	}
+
+	inline void Camera::SetUp(float x, float y, float z) {
+		m_up.Set(x, y, z);
 	}
 
 	inline rVector3 Camera::GetUp() const {
